ac6a0b3860c13119038d8991be45ff80a81ef30d
raheemadamboev <vividricemaryandmax@gmail.com>
2022-11-20 16:22:45 +0500

    install and configure ktlint

--- app/src/main/java/xyz/teamgravity/ktlintdemo/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/ktlintdemo/MainActivity.kt
@@ -17,8 +17,7 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
-                ) {
-
+                ) { // test comment
                 }
             }
         }
